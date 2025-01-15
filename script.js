407f8967d9ee02d455278ed4caa5c460ba095f2a
basurakid <92126346+basurakid@users.noreply.github.com>
2025-01-15 13:41:21 +0100

    complete css and html of modal form

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const myLibrary = [];
 
+const addBookBtn = document.querySelector(".add-book");
+const addBookModal = document.querySelector(".add-book-modal");
+
+const bookForm = document.querySelector(".new-book-form")
+const submitBookBtn = document.querySelector(".submit-book");
+
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -10,4 +16,14 @@ function book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new book(title, author, pages, read)); 
 }
+
+function displayMyLibrary()
+
+addBookBtn.addEventListener("click", () => {
+    addBookModal.showModal();
+})
+
+submitBookBtn.addEventListener("click", () => {
+    inputs bookForm.
+})
 
